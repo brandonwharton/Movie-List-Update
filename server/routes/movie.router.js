281b2258cac3707443e6f7e24ddd639c08563dc4
Brandon Wharton <whartonbm@gmail.com>
2021-06-20 14:03:01 -0500

    Added comments to AddMovie

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
-// GET request for all movies for the home page
+// GET request for all movies for the home page, gathers genre data for each as well
 router.get('/', (req, res) => {
   const query = `
     SELECT ARRAY_AGG ("genres".name), "movies".* FROM "movies"
@@ -19,6 +19,7 @@ router.get('/', (req, res) => {
       res.sendStatus(500)
     })
 });
+
 
 // GET request for a single movie for the Details page along with genre data
 router.get('/:id', (req, res) => {
@@ -41,7 +42,7 @@ router.get('/:id', (req, res) => {
 });
 
 
-
+// add a new movie to the database
 router.post('/', (req, res) => {
   console.log('current req.body', req.body);
   // RETURNING "id" will give us back the id of the created movie
@@ -53,63 +54,47 @@ router.post('/', (req, res) => {
   // FIRST QUERY MAKES MOVIE
   pool.query(insertMovieQuery, [req.body.newMovie.title, req.body.newMovie.poster, req.body.newMovie.description])
     .then(result => {
+      // Now handle the genre reference
+      // save the array of genre data
       let genresArray = req.body.genresArray;
       console.log('New Movie Id:', result.rows[0].id); //ID IS HERE!
-
+      // save the ID of the movie that was just added to put into the junction table
       const createdMovieId = result.rows[0].id
+
       // loop through this process once for each new genre added
       genresArray.forEach(genre => {
         console.log('in for each, genre:', genre);
-        
-        // Now handle the genre reference
+
         const insertMovieGenreQuery = `
         INSERT INTO "movies_genres" ("movie_id", "genre_id")
         VALUES  ($1, $2);
         `
-        // SECOND QUERY ADDS GENRE FOR THAT NEW MOVIE
+        // add the genre id and movie id data into the junction table
         pool.query(insertMovieGenreQuery, [createdMovieId, genre.id]).then(result => {
           //Now that both are done, send back success!
-          console.log('in .then inside loop for genre:', genre);
-          
+          // console.log('in .then inside loop for genre:', genre);
+
+          // I was throwing errors about headers already being sent here, sometimes all
+          // of my genres would add and sometimes the last one would throw the error.
+          // I'm guessing it's bad practice to not have a sendStatus but getting rid of it solved 
+          // the bug and everything seems to work. My thought is that the forEach is moving too quickly and that I should
+          // find a way to have each iteration in it's own .then, but I'm not sure how to handle that.
+          // I would love to learn more.
+
           // res.sendStatus(201);
         }).catch(err => {
           // catch for second query
           console.log(err);
           res.sendStatus(500)
         })
-
       }) // end for each
+
       // Catch for first query
       }).catch(err => {
         console.log(err);
         res.sendStatus(500)
       })
 })
-//     console.log('New Movie Id:', result.rows[0].id); //ID IS HERE!
-
-//     const createdMovieId = result.rows[0].id
-
-//     // Now handle the genre reference
-//     const insertMovieGenreQuery = `
-//       INSERT INTO "movies_genres" ("movie_id", "genre_id")
-//       VALUES  ($1, $2);
-//       `
-//       // SECOND QUERY ADDS GENRE FOR THAT NEW MOVIE
-//       pool.query(insertMovieGenreQuery, [createdMovieId, req.body.genre_id]).then(result => {
-//         //Now that both are done, send back success!
-//         res.sendStatus(201);
-//       }).catch(err => {
-//         // catch for second query
-//         console.log(err);
-//         res.sendStatus(500)
-//       })
-
-// // Catch for first query
-//   }).catch(err => {
-//     console.log(err);
-//     res.sendStatus(500)
-//   })
-// })
 
 
 // PUT request to edit movie details
